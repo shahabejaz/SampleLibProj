232e1e1859af85102b746ea7bb92ccfcc3cd4ac5
Shahab Ejaz <sejaz@folio3.com>
2015-03-20 00:47:26 +0500

    Initial commit of library
    Signed-off-by: Shahab Ejaz <sejaz@folio3.com>

---
+++ SampleLibProj/SampleLibProj.h
@@ -0,0 +1,13 @@
+//
+//  SampleLibProj.h
+//  SampleLibProj
+//
+//  Created by SHAHAB EJAZ on 20/03/2015.
+//  Copyright (c) 2015 Folio3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleLibProj : NSObject
+
+@end
